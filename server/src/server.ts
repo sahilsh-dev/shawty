bba6e9a25a3fda306694d4eec2fdea0e8a443d6b
Sahil Sharma <vlsharma713@gmail.com>
2025-04-09 00:38:23 +0530

    Add mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import mainRoutes from './routes/mainRoutes';
 
-const PORT = process.env.PORT || 8000;
+dotenv.config();
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || '').then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+});
+
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api', mainRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Shawty backend API!');
